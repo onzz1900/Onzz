55ac4b64feedc8745a6afe521f4b046253e20eb0
lipan <lipan@kaytune.com>
2016-11-17 15:26:03 +0800

    AssemblyInfo11

--- src/Onzz/Properties/AssemblyInfo.cs
+++ src/Onzz/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("Onzz")]
-[assembly: AssemblyDescription("onzz工具集")]
+[assembly: AssemblyDescription("onzz工具集1")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("onzz")]
 [assembly: AssemblyProduct("Onzz")]
